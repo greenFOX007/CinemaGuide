91f379f9a83c1cce062ad2e05fc0f9350982993f
Vladimir Mihdalionak <vova280297@gmail.com>
2024-07-04 12:02:42 +0300

    qwerty12345

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -7,16 +7,13 @@ import { EmailSVG, PasswordSVG } from "@/shared/IconsSvg";
 import Input from "@/shared/components/Input/Input";
 import PrimeryButton from "@/shared/components/PrimeryButton/PrimeryButton";
 import Spiner from "@/shared/components/Spiner/Spiner";
-import httpClient from "@/utils/axios";
 import { Formik, Form } from "formik";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { date } from "yup";
 
 export default function AuthForm({ closeModal }: { closeModal: () => void }) {
   const dispatch = useDispatch();
   const { isAuthenticated, authUser } = useAuthSelector();
-  const [dataLol, setDataLol] = useState<any>();
 
   const [
     loginHandler,
@@ -40,49 +37,20 @@ export default function AuthForm({ closeModal }: { closeModal: () => void }) {
               password: value.password,
             };
 
-            let req = await loginHandler({ data: reqValues });
-            if (req.data) {
-              const authUserResponse = await getAuthUser();
-              if (authUserResponse.data) {
-                dispatch(authSlice.actions.authUserData(authUserResponse.data));
-                dispatch(authSlice.actions.loggedIn());
-                setSubmitting(false);
-                closeModal();
-              }
-            }
+            let req = await loginHandler({ data: reqValues })
+              .unwrap()
+              .then(async () => {
+                let authUserResponse = await getAuthUser();
+                if (authUserResponse.data) {
+                  dispatch(
+                    authSlice.actions.authUserData(authUserResponse.data)
+                  );
+                  dispatch(authSlice.actions.loggedIn());
 
-            // closeModal();
-
-            // let req = await loginHandler({ data: reqValues })
-            //   .unwrap()
-            //   .then(async () => {
-            //     let authUserResponse = await getAuthUser();
-            //     setDataLol(authUserResponse.data);
-
-            //     if (authUserResponse.data) {
-            //       dispatch(
-            //         authSlice.actions.authUserData(authUserResponse.data)
-            //       );
-            //       dispatch(authSlice.actions.loggedIn());
-
-            //       // closeModal();
-            //       setStatus("ok");
-            //     }
-            //   });
-            // setSubmitting(false);
-            // let req = await loginHandler({ data: reqValues });
-            // if (req.error) {
-            //   setStatus(req.error);
-            // }
-            // if (isSuccessLogin) {
-            //   // let authUserResponse = await getAuthUser();
-            //   // if (authUserResponse.data) {
-            //   //   dispatch(authSlice.actions.authUserData(authUserResponse.data));
-            //   //   dispatch(authSlice.actions.loggedIn());
-            //   setSubmitting(false);
-            //   closeModal();
-            //   // }
-            // }
+                  closeModal();
+                  setSubmitting(false);
+                }
+              });
           } catch (err) {
             setStatus(err);
             setSubmitting(false);
